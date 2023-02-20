574245e85844e5951d5b0c55b8f89e1eca75d3ef
AmmyAnki <124116595+AmmyAnki@users.noreply.github.com>
2023-02-20 18:38:40 +0530

    Add files via upload

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -39,6 +39,25 @@ const scrollHeader = () => {
 
 /*=============== SWIPER PROJECTS ===============*/
 
+let swiperProjects = new Swiper(".projects__container",{
+  loop:true,
+  spaceBetween: 24,
+
+navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+pagination:{
+  el: ".swiper-pagination",
+},
+breakpoints: {
+  1200: {
+    slidesPerView: 2,
+    spaceBetween: -56,
+  },
+},
+});
+
 /*=============== SWIPER TESTIMONIAL ===============*/
 
 /*=============== EMAIL JS ===============*/
