53a7b2a378e1a0128d7d879f51725a4104330738
AmmyAnki <124116595+AmmyAnki@users.noreply.github.com>
2023-02-23 17:47:04 +0530

    Update main.js

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -61,7 +61,49 @@ breakpoints: {
 /*=============== SWIPER TESTIMONIAL ===============*/
 
 /*=============== EMAIL JS ===============*/
+const contactForm= document.getElementById('contact-form'),
+      contactName= document.getElementById('contact-name'),
+      contactEmail= document.getElementById('contact-email'),
+      contactProject= document.getElementById('contact-project'),
+      contactMessage= document.getElementById('contact-message')
 
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // check if the field has a value
+    if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+      //  Add and remove color
+      contactMessage.classList.remove('color-blue')
+      contactMessage.classList.add('color-red')
+
+      // Show message
+       contactMessage.textContent = 'Write all the input fields 📩'
+    }else{
+      // serviceID - templateID -#form - publicKey
+      emailjs.sendForm('service_bq28b4d','template_7vinp3g','#contact__form','cdB1pf5qUAZs6aG2W')
+      .then(() =>{
+        // Show message and add color
+        contactMessage.classList.add('color-blue')
+        contactMessage.textContent = 'Message sent ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+          contactMessage.textContent = ''
+        }, 5000)
+      }, (error) =>{
+        alert('OOPS! SOMETHING HAS FAILED...', error)
+      })
+
+      // To clear the input field 
+      contactName.value = ''
+      contactEmail.value = ''
+      contactProject.value = ''
+    }
+       
+  }
+
+contactForm.addEventListener('submit',sendEmail)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SHOW SCROLL UP ===============*/
